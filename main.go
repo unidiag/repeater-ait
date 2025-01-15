a5b247c558dd6d4a698193636c7707a16ace2a6d
Vitali Tumasheuski <unidiag@tut.by>
2025-01-15 10:44:49 +0300

    main.go correct

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-	if len(os.Args) != 3 {
+	if len(os.Args) != 3 || len(os.Args) != 4 {
 		color.Green(`	This program retransmits incoming UDP to the next port with the addition of the AIT.
 	Copyright VITALI TUMASHEUSKI aka @unidiag in 2024 (email: tva@tva.by)
 	Usage:		./repeater-ait <udp:port> <hbblink> (pid)
